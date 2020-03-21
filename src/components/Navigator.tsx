a50500786ac2ab596d81c50bf76fbe082536201c
Gonzalo Bide <gonzalo.bide@gmail.com>
2020-03-21 14:13:17 -0300

    Se agrega módulo react bootstrap Mejoras en la pantalla de inicio. Se componentiza el navegador y los botones (para uso futuro)

---
+++ src/components/Navigator.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Btn from './Btn';
+import { TiUser, TiGlobeOutline } from 'react-icons/ti';
+
+const Navigator = (props:any) => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">
+                <TiGlobeOutline size={28} />Educación Digital
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Patio</Nav.Link>
+                <Nav.Link href="#link">Mis Cursos</Nav.Link>
+                <Nav.Link href="#link">Mis Proyectos</Nav.Link>
+                <Nav.Link href="#link">Debates</Nav.Link>
+                <NavDropdown className="mr-right pull-right" title={<TiUser size={32} />} id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Mis datos</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Mi perfil</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Configuraciones</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Acerca de EDi</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="¿Que desea buscar?" className="mr-sm-2" />
+                <Btn variant="outline-success">Buscar</Btn>
+            </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Navigator;
