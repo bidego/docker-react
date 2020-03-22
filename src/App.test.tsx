6192725a924467eefce9f73ed02b7f6b840a0f24
Gonzalo Bide <gonzalo.bide@gmail.com>
2020-03-21 23:24:45 -0300

    Fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { Home } from './screens';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Home />);
   const linkElement = getByText(/entrá?/i);
   expect(linkElement).toBeInTheDocument();
 });
